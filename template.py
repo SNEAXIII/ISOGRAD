b4ab6e09a00ab41a71607eeb29274805aa6a0981
AlexisB <misterbalise2@gmail.com>
2023-10-21 19:31:11 +0200

    add generator for the template

---
+++ template.py
@@ -0,0 +1,18 @@
+import sys, io
+
+sampleToTest = "1"
+with open(f"dataSample/output{sampleToTest}.txt") as f:
+    outputExpected = int(f.read())
+with open(f"dataSample/input{sampleToTest}.txt", "r", encoding="utf-8") as f:
+    sys.stdin = io.StringIO(f.read())
+
+
+# START
+result = ""
+print(result)
+# END
+
+if outputExpected == result:
+    print("Le test est valide")
+else:
+    print(f"Le test n'est pas valide --> output = {result} contre exepct = {outputExpected}")
