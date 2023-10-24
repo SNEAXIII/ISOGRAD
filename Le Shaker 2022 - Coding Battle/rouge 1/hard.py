cd6a13e55a4974bf7468af5dc0b4e62267fd83fd
AlexisB <misterbalise2@gmail.com>
2023-10-24 18:53:06 +0200

    ajout des fonction en cours

--- Le Shaker 2022 - Coding Battle/rouge 1/hard.py
+++ Le Shaker 2022 - Coding Battle/rouge 1/hard.py
@@ -2,17 +2,65 @@ import sys, io
 
 sampleToTest = "1"
 with open(f"dataSample/output{sampleToTest}.txt") as f:
-    outputExpected = int(f.read())
+    outputExpected = f.read()
 with open(f"dataSample/input{sampleToTest}.txt", "r", encoding="utf-8") as f:
     sys.stdin = io.StringIO(f.read())
 
 
 # START
+def maxRecipe(recipe, listTotalBerry):
+    RB1, RB2, RB3, NRJ = recipe
+    TB1, TB2, TB3 = listTotalBerry
+    result = 0
+    if RB1 != 0:
+        result = max(result, TB1 // RB1)
+    if RB2 != 0:
+        result = max(result, TB2 // RB2)
+    if RB3 != 0:
+        result = max(result, TB3 // RB3)
+    return result
+
+
+def buildCounter(listAllRecipe):
+    counter = []
+    for recipe in listAllRecipe:
+        counter.append([0, recipe[4]])
+    return counter
+
+
+def plus1ToCounter(counter):
+    for recipeIndex in counter:
+        pass
+    # TODO Faire le counter
+
+
+def counterToString(counter):
+    pass
+    # TODO A faire
+
+
 result = ""
 print(result)
-# END
 
+listTotalBerry = tuple(map(int, input().split()))
+intNumberOfRecipe = int(input())
+
+listAllRecipe = []
+
+for _ in range(intNumberOfRecipe):
+    recipe = list(map(int, input().split()))
+    recipe.append(maxRecipe(recipe, listTotalBerry))
+    listAllRecipe.append(recipe)
+
+# TODO remove this shit
+print(listAllRecipe)
+
+counter = buildCounter(listAllRecipe)
+
+# TODO remove this shit
+print(counter)
+# END
 if outputExpected == result:
     print("Le test est valide")
 else:
-    print(f"Le test n'est pas valide --> output = {result} contre exepct = {outputExpected}")
+    print(f"Le test n'est pas valide → output = <|{result}|> contre exepct = <|{outputExpected}|>")
