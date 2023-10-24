3c77ec487face4c3d027e8ed612a55ae91869495
AlexisB <misterbalise2@gmail.com>
2023-10-24 14:35:38 +0200

    ajout exercice des baies

---
+++ Le Shaker 2022 - Coding Battle/rouge 1/partialInput.py
@@ -0,0 +1,7 @@
+import sys, io
+
+sampleToTest = "10"
+with open(f"output{sampleToTest}.txt") as f:
+    outputExpected = f.read()
+with open(f"input{sampleToTest}.txt", "r", encoding="utf-8") as f:
+    sys.stdin = io.StringIO(f.read())
