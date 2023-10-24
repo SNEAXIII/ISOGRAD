2471c9c7bb6b88c3513f3577a8ef170aec6beafb
AlexisB <misterbalise2@gmail.com>
2023-10-24 16:08:26 +0200

    fix debug function

--- template.py
+++ template.py
@@ -5,14 +5,11 @@ with open(f"dataSample/output{sampleToTest}.txt") as f:
     outputExpected = f.read()
 with open(f"dataSample/input{sampleToTest}.txt", "r", encoding="utf-8") as f:
     sys.stdin = io.StringIO(f.read())
-
-
 # START
 result = ""
 print(result)
 # END
-
 if outputExpected == result:
     print("Le test est valide")
 else:
-    print(f"Le test n'est pas valide --> output = {result} contre exepct = {outputExpected}")
+    print(f"Le test n'est pas valide → output = <|{result}|> contre exepct = <|{outputExpected}|>")
