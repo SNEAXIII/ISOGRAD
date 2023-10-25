acc2f1ac26b48b23d2f6d0d239727e1680dc16ac
AlexisB <misterbalise2@gmail.com>
2023-10-25 18:56:12 +0200

    build class counter

--- Le Shaker 2022 - Coding Battle/rouge 1/hard.py
+++ Le Shaker 2022 - Coding Battle/rouge 1/hard.py
@@ -21,41 +21,45 @@ def maxRecipe(recipe, listTotalBerry):
     return result
 
 
-def buildCounter(listAllRecipe):
-    counter = []
-    for recipe in listAllRecipe:
-        counter.append([0, recipe[4]])
-    return counter
+class counter:
+    def __init__(self, listAllRecipe):
+        self.counter = self.buildCounter(listAllRecipe)
+
+    def buildCounter(self, listAllRecipe):
+        counter = []
+        for recipe in listAllRecipe:
+            counter.append([0, recipe[4]])
+        return counter
+
+    @staticmethod
+    def listWithIntToHex(listeRecipeIndex):
+        if listeRecipeIndex[0] > 15:
+            print("-------------c'est la merde--------------")
+            print("------------oublie la base 16------------")
+        return format(listeRecipeIndex[0], "x")
+
+    def counterToString(self):
+        return "".join(map(self.listWithIntToHex, self.counter))
+
+    def isEmptyCounter(self):
+        return self.counterToString() == "0" * len(self.counter)
+
+    def plus1ToCounter(self):
+        for recipeIndex in self.counter:
+            recipeIndex[0] += 1
+            if not recipeIndex[0] > recipeIndex[1]:
+                return True
+            recipeIndex[0] = 0
+        return False
 
 
-def listWithIntToHex(listeRecipeIndex):
-    if listeRecipeIndex[0] > 15:
-        print("-------------c'est la merde--------------")
-        print("------------oublie la base 16------------")
-    return format(listeRecipeIndex[0], "x")
-
-
-def plus1ToCounter(counter):
-    for recipeIndex in counter:
-        recipeIndex[0]+=1
-        if not recipeIndex[0] > recipeIndex[1]:
-            return True
-        recipeIndex[0] = 0
-    return False
-
-# TODO c'est tres sus comme technique faire gaffe aux reset pas et qui fait des boucles infinies
-def getToPreviousRecipe(counter):
-    for index,recipeCounter in enumerate(counter[::-1]):
-        if recipeCounter[0]:
-            recipeCounter[0] = 0
-            plus1ToCounter(counter)
-            break
-
-def counterToString(counter):
-    return "".join(map(listWithIntToHex, counter))
-
-def isEmptyCounter(counter):
-    return counterToString(counter) == "0" * len(counter)
+    # TODO c'est tres sus comme technique faire gaffe aux reset pas et qui fait des boucles infinies
+    def getToPreviousRecipe(self):
+        for index, recipeCounter in enumerate(self.counter[::-1]):
+            if recipeCounter[0]:
+                recipeCounter[0] = 0
+                self.plus1ToCounter()
+                break
 
 
 result = ""
@@ -75,26 +79,26 @@ for _ in range(intNumberOfRecipe):
 # TODO remove this shit
 print(listAllRecipe)
 
-counter = buildCounter(listAllRecipe)
+counter = counter(listAllRecipe)
 
 print("1_______________________")
-print(counter)
-plus1ToCounter(counter)
-plus1ToCounter(counter)
-plus1ToCounter(counter)
-plus1ToCounter(counter)
-plus1ToCounter(counter)
-plus1ToCounter(counter)
-plus1ToCounter(counter)
-plus1ToCounter(counter)
-plus1ToCounter(counter)
+print(counter.counterToString())
+counter.plus1ToCounter()
+# plus1ToCounter(counter)
+# plus1ToCounter(counter)
+# plus1ToCounter(counter)
+# plus1ToCounter(counter)
+# plus1ToCounter(counter)
+# plus1ToCounter(counter)
+# plus1ToCounter(counter)
+# plus1ToCounter(counter)
 print("2_______________________")
-print(counter)
+print(counter.counterToString())
 
 # print(counterToString(counter))
 print("3_______________________")
-getToPreviousRecipe(counter)
-print(counter)
+# getToPreviousRecipe(counter)
+print(counter.counter)
 
 # END
 if outputExpected == result:
