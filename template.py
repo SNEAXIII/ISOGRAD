8307e80c3216b177e437bdb8615e70b4a877b00a
AlexisB <misterbalise2@gmail.com>
2023-10-25 23:36:01 +0200

    fix template

--- template.py
+++ template.py
@@ -12,4 +12,4 @@ print(result)
 if outputExpected == result:
     print("Le test est valide")
 else:
-    print(f"Le test n'est pas valide → output = <|{result}|> contre exepct = <|{outputExpected}|>")
+    print(f"Le test n'est pas valide\noutput ----> <|{result}|>\nexepct ----> <|{outputExpected}|>")
