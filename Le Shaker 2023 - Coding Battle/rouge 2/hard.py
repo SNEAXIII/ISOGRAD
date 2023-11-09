4fa11369525394abf6dc38bffd1b83cbcd73395b
Alexis Bassignot <misterbalise2@gmail.com>
2023-11-09 13:56:42 +0100

    push de 14h

--- Le Shaker 2023 - Coding Battle/rouge 2/hard.py
+++ Le Shaker 2023 - Coding Battle/rouge 2/hard.py
@@ -1,17 +1,32 @@
 import sys, io
-from testVisu import main
+from testVisu import show
 
-sampleToTest = "2"
+sampleToTest = "Custom"
 with open(f"dataSample/output{sampleToTest}.txt") as f:
     outputExpected = f.read()
 with open(f"dataSample/input{sampleToTest}.txt", "r", encoding="utf-8") as f:
     sys.stdin = io.StringIO(f.read())
 
-
 # START
+TOP = 0
+RIGHT = 1
+BOTTOM = 2
+LEFT = 3
 
-def printGrid():
-    main(gridForest)
+
+class Pile:
+    def __init__(self):
+        self.pile = []
+
+    def depile(self):
+        return self.pile.pop()
+
+    def empile(self, x, y):
+        self.pile.append((x, y))
+
+
+def printGrid(xCurrent, yCurrent):
+    show(gridForest, xCurrent, yCurrent)
 
 
 def addTree(xTree, yTree, hTree):
@@ -27,37 +42,120 @@ def addMultiTree():
 
 
 def isEnoughReachToJoinTheExit(x, y, h):
-    return x + y - 2 <= h
+    return width - x + height - y - 2 <= h
 
 
-def coordinate_to_index(x, y, height):
-    return y * height + x
+def lockResetCell(x, y):
+    if x == 0:
+        lockside(x, y, LEFT, False)
+    if x == width - 1:
+        lockside(x, y, RIGHT, False)
+    if y == 0:
+        lockside(x, y, TOP, False)
+    if y == height - 1:
+        lockside(x, y, BOTTOM, False)
+
+
+def lockside(x, y, side, alert=True):
+    if not isValidToVisit(x, y, side) and alert:
+        print("ALEEEEED")
+    gridParcours[y][x][side] = 1
+
+
+def isValidToVisit(x, y, side):
+    if gridParcours[y][x][side]:
+        return False
+    return True
+
+
+def isAlreadyVisited(cell):
+    if cell in pile.pile:
+        return True
+    return False
+
+
+def findCoordsByASide(x, y, side):
+    if side == 0:
+        y -= 1
+    elif side == 1:
+        x += 1
+    elif side == 2:
+        y += 1
+    elif side == 3:
+        x -= 1
+    return x, y
+
+
+def chooseSideToGo(x, y):
+    for side in range(4):
+        if isValidToVisit(x, y, side):
+            newCoords = findCoordsByASide(x, y, side)
+            if not isAlreadyVisited(newCoords):
+                return *newCoords, side
+    return False
+
+
+def mooveToNextCell(xCurrent, yCurrent, xNew, yNew, side):
+    # On bloque la possibilité d'aller à la case ou l'on va
+    lockside(xCurrent, yCurrent, side)
+    # On prends le coté opposé
+    reversedSide = reverseSide(side)
+    # On bloque la possibilité d'aller à la case ou l'on est actuellement
+    lockside(xNew, yNew, reversedSide)
+    # On enregistre la case ou l'on se dirige
+    pile.empile(xNew, yNew)
+    # On enregistre le déplacement
+    return xNew, yNew
+
+def reverseSide(side):
+    return (side + 2) % 4
+
+
+def voyage(hPlanneur):
+    xCurrent, yCurrent = 0, 0
+    pile.empile(xCurrent, yCurrent)
+    # todo start fix
+    coordsPlusSideToGO = chooseSideToGo(xCurrent, yCurrent)
+    xCurrent, yCurrent = mooveToNextCell(xCurrent, yCurrent, *coordsPlusSideToGO)
+    # todo end fix
+    print(coordsPlusSideToGO,hPlanneur)
+    print(pile.pile)
+    printGrid(xCurrent, yCurrent)
 
 
 # TODO montrer au prof l'erreur qu'il a remarque sur le jeu numéro 2
 width, height = map(int, input().split())
-h = int(input())
-print(h)
+hPlanneur = int(input())
+
+
 
 # Si le parcours est impossible
-if not isEnoughReachToJoinTheExit(width, height, h):
-    printGrid()
-    print("impossible")
-    exit()
+# TODO remettre plus tard si il le faut
+# if not isEnoughReachToJoinTheExit(width, height, hPlanneur):
+#     printGrid()
+#     print("impossible")
+#     exit()
 
-gridForest = [[0] * width] * height
+gridForest = [[0 for _ in range(width)] for _ in range(height)]
 
 addMultiTree()
-# todo faire une matrice des coté emprunté pour chaque cases individuelles
 # todo faire un fonction pour déterminer un coté ou partir
 # todo faire une fonction pour savoir si on est deja allé
 # todo faire un roll back
-gridParcours = [[[0] * 4] * width] * height
+gridParcours = []
+
+for yh in range(height):
+    gridParcours.append([])
+    for xw in range(width):
+        gridParcours[yh].append([0, 0, 0, 0])
+        lockResetCell(xw, yh)
+
 for line in gridParcours:
     print(line)
 
-# todo faire un set des case déja parcourues
-printGrid()
+pile = Pile()
+
+voyage(hPlanneur)
 
 result = ""
 print(result)
--- Le Shaker 2023 - Coding Battle/rouge 2/testVisu.py
+++ Le Shaker 2023 - Coding Battle/rouge 2/testVisu.py
@@ -7,7 +7,10 @@ WINDOW_WIDTH = WINDOW_HEIGHT = 800
 CELL_SIZE = 50
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
-def draw_grid(screen, data):
+RED = (255, 0, 0)
+
+
+def draw_grid(screen, data, xCurrent, yCurrent):
     screen.fill(WHITE)
     for x, y, h in data:
         pygame.draw.rect(screen, BLACK, (x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE))
@@ -15,11 +18,15 @@ def draw_grid(screen, data):
         text = font.render(str(h), True, WHITE)
         text_rect = text.get_rect(center=(x * CELL_SIZE + CELL_SIZE / 2, y * CELL_SIZE + CELL_SIZE / 2))
         screen.blit(text, text_rect)
+        if x == xCurrent and y == yCurrent:
+            deltaplane = pygame.draw.circle(screen, RED,
+                                            (x * CELL_SIZE + CELL_SIZE / 2, y * CELL_SIZE + CELL_SIZE / 2), 3)
 
-def main(rawData):
+
+def show(rawDataForest, xCurrent, yCurrent):
     data = []
-    for y, row in enumerate(rawData):
-        for x, h in enumerate(row):
+    for y, line in enumerate(rawDataForest):
+        for x, h in enumerate(line):
             data.append((x, y, h))
     screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
     pygame.display.set_caption("Quadrillage Pygame")
@@ -30,5 +37,5 @@ def main(rawData):
                 pygame.quit()
                 sys.exit()
 
-        draw_grid(screen, data)
+        draw_grid(screen, data, xCurrent, yCurrent)
         pygame.display.flip()
