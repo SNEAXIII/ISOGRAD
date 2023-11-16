0c72db534b66e5bb56a320dcee474d77384c991f
Alexis Bassignot <misterbalise2@gmail.com>
2023-11-16 13:21:20 +0100

    finish exo vert

---
+++ Concours La Poste Final/orange 1/template.py
@@ -0,0 +1,16 @@
+import sys, io
+
+sampleToTest = "1"
+with open(f"dataSample/output{sampleToTest}.txt") as f:
+    outputExpected = f.read()
+with open(f"dataSample/input{sampleToTest}.txt", "r", encoding="utf-8") as f:
+    sys.stdin = io.StringIO(f.read())
+# START
+result = ""
+
+print(result)
+# END
+if outputExpected == result:
+    print("Le test est valide")
+else:
+    print(f"Le test n'est pas valide\noutput ----> <|{result}|>\nexepect ----> <|{outputExpected}|>")
