1efeba07416beda72fa4458a3172e1b53bc74101
Alexis Bassignot <misterbalise2@gmail.com>
2023-11-27 17:02:23 +0100

    ca avance....

--- Concours La Poste Final/orange 1/template.py
+++ Concours La Poste Final/orange 1/template.py
@@ -1,6 +1,6 @@
 import sys, io
 
-sampleToTest = "Custom"
+sampleToTest = "Custom2"
 with open(f"dataSample/output{sampleToTest}.txt") as f:
     outputExpected = f.read()
 with open(f"dataSample/input{sampleToTest}.txt", "r", encoding="utf-8") as f:
@@ -11,19 +11,41 @@ lStatue, hStatue, lSalle, hSalle = map(int, input().split(" "))
 
 lines = [input() for _ in range(hSalle)]
 linesDyna = []
-for indexLine,line in enumerate(lines):
+colonnesDyna = []
+print("les lignes")
+for y, yElem in enumerate(lines):
     print("______________________")
     count = 0
     linesDyna.append(set())
-    for index,space in enumerate(line):
-        if space == ".":
+    for x, xElem in enumerate(yElem):
+        if xElem == ".":
             count += 1
-        elif space == "#":
+        elif xElem == "#":
             count = 0
-        if count >= lStatue+2:
-            linesDyna[indexLine].add(index)
-            print(index)
+        if count >= lStatue + 2:
+            xGood = x - lStatue
+            linesDyna[y].add(xGood)
+            print(xGood)
+
+print("les collones")
+for x in range(lSalle):
+    print("______________________")
+    count = 0
+    colonnesDyna.append(set())
+    for y in range(hSalle):
+        case = lines[y][x]
+        if case == ".":
+            count += 1
+        elif case == "#":
+            count = 0
+        if count >= hStatue + 2:
+            yGood = y - hStatue
+            colonnesDyna[x].add(yGood)
+            print(xGood)
+
 print(linesDyna)
+print(colonnesDyna)
+
 
 print(result)
 # END
